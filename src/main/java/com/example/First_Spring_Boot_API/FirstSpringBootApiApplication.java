9ac0e978ca138b2c62b7d15f196585e36711ccda
Padiaten <dai1600@uom.edu.gr>
2022-05-13 14:41:51 +0300

    First Commit

---
+++ src/main/java/com/example/First_Spring_Boot_API/FirstSpringBootApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.First_Spring_Boot_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringBootApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringBootApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/First_Spring_Boot_API/FirstSpringBootApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.First_Spring_Boot_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringBootApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
